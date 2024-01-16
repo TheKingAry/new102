68566af04d1beb639b0f412ef08c516d1c6f50f2
AryStorm <71211293+TheKingAry@users.noreply.github.com>
2024-01-16 20:46:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import os 
+import shutil
+source=r'C:\Users\ZEBRONICS01\Downloads\C102_assets-main'
+destination=r'C:\Users\ZEBRONICS01\Downloads\C102_assets-main\Images'
+list_of_files = os.listdir(source)
+
+for files in list_of_files:
+    name,extension=os.path.splitext(files)
+    if extension == "":
+        continue
+    if extension in [".gif",".png",".jfif",".jpg"]:
+        path1= source+"/"+files
+        path2=destination
+        path3=path2+"/"+files
+        if os.path.exists(path2):
+            shutil.move(path1,path3)
+        else:
+            os.mkdir(path2)
+            shutil.move(path1,path3)
